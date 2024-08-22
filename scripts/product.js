337559204160fbf9acee6458bd6d9796624d3a18
Mujeeb <mujeebabiola898@gmail.com>
2024-08-22 17:19:11 +0100

    moved some pics from fashio to products

--- scripts/product.js
+++ scripts/product.js
@@ -1,6 +1,15 @@
 const galleryArray = [
     {image: "https://i.ibb.co/gWNDYj7/212.jpg"},
     {image: "https://i.ibb.co/m03vBhr/309.jpg"},
+    {image: "https://i.ibb.co/fYFf9kN/image00001.jpg"},
+    {image: "https://i.ibb.co/YyFQX9S/image00002.jpg"},
+    {image: "https://i.ibb.co/bXLQmJh/image00003.jpg"},
+    {image: "https://i.ibb.co/34F6mfh/image00004.jpg"},
+    {image: "https://i.ibb.co/Y0dnvF1/image00005.jpg"},
+    {image: "https://i.ibb.co/PrvJ2K5/image00006.jpg"},
+    {image: "https://i.ibb.co/9WBZ9s6/image00007.jpg"},
+    {image: "https://i.ibb.co/6n3SDZZ/image00008.jpg"},
+    {image: "https://i.ibb.co/LxH3DPS/image00009.jpg"},
     {image: 'https://i.ibb.co/dQcQ0T8/image00001.jpg'},
     {image: "https://i.ibb.co/jWN4SVz/image00002.jpg"},
     {image: "https://i.ibb.co/pds115D/image00006.jpg"},
@@ -17,7 +26,7 @@ const galleryArray = [
     {image: "https://i.ibb.co/PZrmMC7/PRs-3.jpg"},
     {image: "https://i.ibb.co/P902XCL/PRs-7.jpg" },
     {image: "https://i.ibb.co/XFMZ1P4/zay-2.jpg"},
-    {image: "https://i.ibb.co/wC8tRT2/zay.jpg"},
+    {image: "https://i.ibb.co/wC8tRT2/zay.jpg"}, 
 ]
 
 let galleryHtml = '';
